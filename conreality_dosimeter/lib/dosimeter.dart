f23b30acc35fc6753f20a09f1dddb0f5b629e901
Arto Bendiken <arto@conreality.org>
2019-07-01 06:40:44 +0300

    Defined the Dosimeter widget.

--- conreality_dosimeter/lib/dosimeter.dart
+++ conreality_dosimeter/lib/dosimeter.dart
@@ -1,7 +1,37 @@
+/* This is free and unencumbered software released into the public domain. */
+
 library dosimeter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async' show Timer;
+
+import 'package:flutter/material.dart';
+
+class Dosimeter extends StatefulWidget {
+  Dosimeter({Key key}) : super(key: key);
+
+  @override
+  State<Dosimeter> createState() => _DosimeterState();
+}
+
+class _DosimeterState extends State<Dosimeter> {
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return Text("TODO"); // TODO
+  }
 }
