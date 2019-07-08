6b1fcd04689cdfc9d749d48fa3a4979e65876fbf
Arto Bendiken <arto@conreality.org>
2019-07-08 22:07:16 +0300

    Added rotation gestures to the Scene widget.

--- conreality_scene/lib/src/scene.dart
+++ conreality_scene/lib/src/scene.dart
@@ -4,7 +4,7 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-class Scene extends StatelessWidget {
+class Scene extends StatefulWidget {
   const Scene(
       {Key key,
       this.crossAxisCount = 32,
@@ -17,27 +17,40 @@ class Scene extends StatelessWidget {
   final Color tileColor;
 
   @override
+  State<Scene> createState() => _SceneState();
+}
+
+class _SceneState extends State<Scene> {
+  Offset _offset = Offset.zero;
+
+  @override
   Widget build(final BuildContext context) {
     return Container(
       alignment: Alignment.bottomCenter,
       child: Transform(
         alignment: Alignment.center,
         transform: Matrix4.identity()
-          ..setEntry(3, 2, 0.003) // perspective
-          ..rotateX(-(math.pi / 3)),
-        child: GridView.count(
-          crossAxisCount: crossAxisCount,
-          children: List<Widget>.generate(crossAxisCount * crossAxisCount, (_) {
-            return Container(
-              decoration: BoxDecoration(
-                color: tileColor.withOpacity(0.5),
-                border: Border.all(
-                  width: 0.5,
-                  color: gridColor,
+          ..setEntry(3, 2, 0.003) // perspective transform
+          ..rotateX(-(math.pi / 3) + 0.01 * _offset.dy)
+          ..rotateY(-0.01 * _offset.dx),
+        child: GestureDetector(
+          onDoubleTap: () => setState(() => _offset = Offset.zero),
+          onPanUpdate: (details) => setState(() => _offset += details.delta),
+          //onScaleUpdate: (details) => setState(() {}), // TODO: zoom
+          child: GridView.count(
+            crossAxisCount: widget.crossAxisCount,
+            children: List<Widget>.generate(widget.crossAxisCount * widget.crossAxisCount, (_) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: widget.tileColor.withOpacity(0.5),
+                  border: Border.all(
+                    width: 0.5,
+                    color: widget.gridColor,
+                  ),
                 ),
-              ),
-            );
-          }),
+              );
+            }),
+          ),
         ),
       ),
     );
